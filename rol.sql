f73ea7c37d127d19d5198d859aa4f7a3e826c3af
Your Name <you@example.com>
2021-12-08 18:16:11 +0100

    rol

---
+++ rol.sql
@@ -0,0 +1,14 @@
+CREATE TABLE personajes (
+    ID int NOT NULL,
+    nombre varchar(255) NOT NULL,
+    caracteristicas varchar(255),
+    id_persona int,
+    PRIMARY KEY (ID)
+);
+
+create table personas (
+    nombre VARCHAR(255),
+    id INT
+);
+
+create table
